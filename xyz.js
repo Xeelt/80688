e948320754868d4b57a6505031da7ec1de624eb4
Yhocsan Uzzi <112590955+Xeelt@users.noreply.github.com>
2024-10-03 14:09:09 -0600

    Add files via upload

---
+++ xyz.js
@@ -0,0 +1,10 @@
+import * as miModulo from "./x.js"
+import {y} from "./y.js"
+import {z} from "./z.js"
+
+const app = document.getElementById("app")
+app.innerHTML = "App"
+console.log(miModulo.x("123"))
+console.log(miModulo.xx("qwer"))
+console.log(y("asd"))
+console.log(z("abc"))
